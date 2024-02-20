d8049f0e97865ffc29262a78480b16a55b5238e5
Alex Burke <156821649+TheBurke24@users.noreply.github.com>
2024-02-20 10:07:29 -0500

    trafficlightCode.ino

--- Traffic Light/trafficlightCode.ino
+++ Traffic Light/trafficlightCode.ino
@@ -3,28 +3,31 @@
 //Encountered many issues with traffic light sequencing, as well as the button state being checked as frequently as necessary.
 
 // C++ code
+//
+ int buttonState = 0;
+
 void setup()
 {
+  pinMode(0, OUTPUT);
   pinMode(1, OUTPUT); //red 1
   pinMode(2, OUTPUT); //yellow 1 
   pinMode(3, OUTPUT); // green 1
   pinMode(4, OUTPUT); //green 2
   pinMode(5, OUTPUT); //yellow 2 
   pinMode(6, OUTPUT); //red 2
-  pinMode(10, INPUT); //crosswalk button
-  int buttonState = 0;
+  pinMode(10, INPUT); //button
+ 
 }
 
 
 void loop()
 {
   digitalWrite(1, HIGH);  // Traffic Light 1
-  digitalWrite(4, HIGH);  // Traffic Light 2
+  digitalWrite(4, HIGH);
   delay(5000);             
   digitalWrite(1, LOW);
   digitalWrite(4, LOW);   
 
-checkButton();
 
   digitalWrite(3, HIGH);
   digitalWrite(5, HIGH);  
@@ -32,24 +35,27 @@ checkButton();
   digitalWrite(3, LOW);
   digitalWrite(5, LOW);   
 
-checkButton();
+checkButton(); // Check for button press
 
   digitalWrite(2, HIGH);
   digitalWrite(6, HIGH);  
   delay(5000);                      
   digitalWrite(2, LOW);
   digitalWrite(6, LOW); 
+
+
 }
 
 // check button
-void checkButton()
-{
+void checkButton(){
   buttonState = digitalRead(10);
   if(buttonState == LOW) {
+    digitalWrite(0, HIGH); //Turn on crossing light
     digitalWrite(1, HIGH);
-    digitalWrite(4, HIGH);
-    delay(2000);
+    digitalWrite(6, HIGH);
+    delay(3000);
+    digitalWrite(0, LOW);
     digitalWrite(1, LOW);
-    digitalWrite(4, LOW);
+    digitalWrite(6, LOW);
   }
 }
